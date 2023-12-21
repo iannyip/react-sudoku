2785dc3f00369003d3c614ba37f7b8e77cbddde2
iannyip <ianyipsh@gmail.com>
2023-12-21 21:33:12 +0800

    store selected cell logic at game level

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,14 +1,35 @@
 import sudoku from '../controllers/game.js';
 import Cell from './cell.jsx';
+import { useState } from 'react';
 import './styles.css';
 
 export default function Game() {
-  console.log(sudoku);
-  console.log(sudoku.puzzle.split(''));
+  const [cellSelection, setCellSelection] = useState({
+    isSelected: false,
+    coordinates: { xValue: 0, yValue: 0 },
+  });
+
+  const updateSelectedCell = (cell) => {
+    setCellSelection({
+      isSelected: true,
+      coordinates: cell.getCoordinates(),
+    });
+  };
+
+  console.log(cellSelection);
+
   return (
     <div className='game-board'>
       {sudoku.puzzle.split('').map((val, index) => {
-        return <Cell key={index} index={index} val={val} />;
+        return (
+          <Cell
+            key={index}
+            index={index}
+            val={val}
+            cellSelection={cellSelection}
+            updateSelectedCell={updateSelectedCell}
+          />
+        );
       })}
     </div>
   );
