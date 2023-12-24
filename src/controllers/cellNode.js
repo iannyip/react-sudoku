e92866b885226d4ca5f1b3ba8eb509d04e99af46
iannyip <ianyipsh@gmail.com>
2023-12-24 15:14:10 +0800

    moved border styling logic to cell class

--- src/controllers/cellNode.js
+++ src/controllers/cellNode.js
@@ -4,6 +4,38 @@ const getCoordinates = (index) => {
   return { xValue, yValue };
 };
 
+const checkBorderY = (cellIndex) => {
+  switch (true) {
+    case cellIndex < 9:
+      return 'outer-top';
+    case cellIndex >= 72:
+      return 'outer-bottom';
+    case (18 <= cellIndex && cellIndex <= 26) ||
+      (45 <= cellIndex && cellIndex <= 53):
+      return 'inner-bottom';
+    case (27 <= cellIndex && cellIndex <= 35) ||
+      (54 <= cellIndex && cellIndex <= 62):
+      return 'inner-top';
+    default:
+      return 'none';
+  }
+};
+
+const checkBorderX = (cellIndex) => {
+  switch (true) {
+    case cellIndex % 9 === 0:
+      return 'outer-left';
+    case (cellIndex + 1) % 9 === 0:
+      return 'outer-right';
+    case (cellIndex - 2) % 9 === 0 || (cellIndex - 5) % 9 === 0:
+      return 'inner-right';
+    case (cellIndex - 3) % 9 === 0 || (cellIndex - 6) % 9 === 0:
+      return 'inner-left';
+    default:
+      return 'none';
+  }
+};
+
 class CellNode {
   constructor(index, value) {
     this.index = index;
@@ -24,6 +56,10 @@ class CellNode {
     } else {
       return false;
     }
+  }
+
+  get getBorderStyles() {
+    return `${checkBorderY(this.index)} ${checkBorderX(this.index)}`;
   }
 
   get getValue() {
